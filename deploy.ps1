c4e6a0652e00b5cf04b9b368695630fe667866b0
Zackery Ayscue <ztayscue@gmail.com>
2020-11-20 18:00:25 -0500

    Added the deploy ps script and the VPC in the cf.

---
+++ deploy.ps1
@@ -0,0 +1,6 @@
+#! /usr/bin/pwsh
+
+$folderName = $pwd | Select-Object | %{$_.ProviderPath.Split("\")[-1]}
+$templateFilePath = ($pwd).Path + "/fargate.yml"
+
+aws cloudformation deploy --template-file $templateFilePath --stack-name $folderName
